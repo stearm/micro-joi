82c2df48700630043fb66ae62fcf0ecada291ee5
Stefano Armenes <stefano.armenes@gmail.com>
2017-10-28 14:05:44 +0200

    query params validation: if query field exists in Joi validation schema, then body field must be used to validate also the body. If no, fallback on entire Joi schema as request body

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
-const { json, send } = require('micro')
-const Joi = require('joi')
+const { json, send } = require('micro');
+const Joi = require('joi');
+const url = require('url');
 
 module.exports = exports = (schema, message) => fn => {
   if (!schema) {
-    throw Error('joi schema required.')
+    throw Error('joi schema required.');
   }
 
   if (!fn || typeof fn !== 'function') {
-    throw Error('a function is required.')
+    throw Error('a function is required.');
   }
 
   return async (req, res) => {
-    const body = await json(req)
+    const queryParameters = url.parse(req.url, true).query;
+    const querySchema = Joi.reach(schema, 'query');
+    const bodySchema = Joi.reach(schema, 'body');
 
-    const result = Joi.validate(body, schema)
-    
-    if (result.error) {
-      send(res, 400, message || result.error.details)
-      return
+    try {
+      const body = await json(req);
+      const result = Joi.validate(bodySchema ? bodySchema : body, schema);
+
+      if (result.error) {
+        send(res, 400, message || result.error.details);
+        return;
+      }
+
+      req.body = result.value;
+    } catch (err) {}
+
+    if (querySchema) {
+      const result = Joi.validate(queryParameters, querySchema);
+      if (result.error) {
+        send(res, 400, message || result.error.details);
+        return;
+      }
     }
-    
-    req.body = result.value
-
-    return fn(req, res)
-  }
-}
+    return fn(req, res);
+  };
+};
